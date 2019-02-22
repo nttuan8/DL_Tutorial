01036c81e0ef43e8e352d2946dfe3cbae44097fc
gwthanhtuanvp88 <tuanntgt00538@fpt.edu.vn>
2019-02-22 21:32:02 +0000

    Linear regression

---
+++ l1.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 22:06:34 2019
+
+@author: DELL
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#numOfPoint = 30
+#noise = np.random.normal(0,1,numOfPoint).reshape(-1,1)
+#x = np.linspace(30, 100, numOfPoint).reshape(-1,1)
+#N = x.shape[0]
+#y = 15*x + 8 + 20*noise
+#plt.scatter(x, y)
+
+data = pd.read_excel('d1.xls').values
+N = data.shape[0]
+x = data[:, 0].reshape(-1, 1)
+y = data[:, 1].reshape(-1, 1)
+plt.scatter(x, y)
+plt.xlabel('mét vuông')
+plt.ylabel('giá')
+plt.show()
+
+x = np.hstack((np.ones((N, 1)), x))
+
+w = np.array([0.,1.]).reshape(-1,1)
+
+numOfIteration = 100
+cost = np.zeros((numOfIteration,1))
+learning_rate = 0.00001
+for i in range(1, numOfIteration):
+    r = np.dot(x, w) - y
+    cost[i] = 0.5*np.sum(r*r)/N
+    w[0] -= learning_rate*np.sum(r)/N
+    w[1] -= learning_rate*np.sum(np.multiply(r, x[:,1]))/N
+    print(cost[i])
+predict = np.dot(x, w)
+plt.plot((x[0][1], x[N-1][1]),(predict[0], predict[N-1]), 'r')
+plt.show()
