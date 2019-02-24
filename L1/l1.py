0e59f548291605d16c635cf7808e6741d89d4ed5
gwthanhtuanvp88 <tuanntgt00538@fpt.edu.vn>
2019-02-24 10:35:10 +0000

    Dữ liệu và code bài 1

--- L1/l1.py
+++ L1/l1.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 #y = 15*x + 8 + 20*noise
 #plt.scatter(x, y)
 
-data = pd.read_excel('d1.xls').values
+data = pd.read_csv('data_linear.csv').values
 N = data.shape[0]
 x = data[:, 0].reshape(-1, 1)
 y = data[:, 1].reshape(-1, 1)
@@ -28,15 +28,19 @@ x = np.hstack((np.ones((N, 1)), x))
 
 w = np.array([0.,1.]).reshape(-1,1)
 
-numOfIteration = 100
+numOfIteration = 20
 cost = np.zeros((numOfIteration,1))
-learning_rate = 0.00001
+learning_rate = 0.0000001
 for i in range(1, numOfIteration):
     r = np.dot(x, w) - y
-    cost[i] = 0.5*np.sum(r*r)/N
-    w[0] -= learning_rate*np.sum(r)/N
-    w[1] -= learning_rate*np.sum(np.multiply(r, x[:,1]))/N
+    cost[i] = 0.5*np.sum(r*r)
+    w[0] -= learning_rate*np.sum(r)
+    w[1] -= learning_rate*np.sum(np.multiply(r, x[:,1]))
     print(cost[i])
 predict = np.dot(x, w)
 plt.plot((x[0][1], x[N-1][1]),(predict[0], predict[N-1]), 'r')
 plt.show()
+
+x1 = 50
+y1 = w[0] + w[1] * 50
+print('Giá nhà cho 50m^2 là : ', y1)
